9d4f0cd03fade6318ca3fc91c69169bad9bfff25
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 21:22:29 +0000

    returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""2-floor.py: function to_str that takes a float n as argument and returns the
+   string representation of the float.
+"""
+
+
+def floor(n: float) -> float:
+    """Get the value of the floor of the float.
+
+    Args:
+        n (float): floating number
+
+    Returns:
+        float: returns the floor of the float.
+    """
+    from math import floor
+    return floor(n)
