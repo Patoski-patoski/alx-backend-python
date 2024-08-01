60e3c48ad53bbc44ebde07b5a6eddbd0a21514bb
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 21:05:58 +0000

    that takes a float a and a float b as arguments and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+"""main.py: contains a function that adds two floating arguments """
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating-point numbers.
+
+    This function takes two float arguments and returns their sum as a float.
+
+    Args:
+        a (float): The first number to be added.
+        b (float): The second number to be added.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
