e952cda10358fc8851e8507720694a91da23539e
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 22:48:14 +0000

    takes a float n as argument and returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""3-to_str.py: function to_str that takes a float n as argument and returns
+  the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Get the string representaion of float value
+
+    Args:
+        n (float): the number to convert to string
+
+    Returns:
+        str: returns the string representation of the float.
+    """
+    return str(n)
