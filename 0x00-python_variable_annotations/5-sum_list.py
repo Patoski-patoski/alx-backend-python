6311cc4ba7cb8343102c64e627d813d78dd762e7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 23:21:39 +0000

    takes a list input_list of floats as argument and returns their sum as a float.

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,4 +6,13 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """takes a list input_list of floats as argument and returns their sum as a
+       float.
+
+    Args:
+        input_list (List[float]): annotated list
+
+    Returns:
+        float: sum of the floats in the list
+    """
     return sum(input_list)
