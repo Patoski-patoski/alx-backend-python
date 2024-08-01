501f53e9f81f5bb47b212949570282f62c652502
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 21:12:38 +0000

    returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+"""1-concat.py:  function concat that takes a string str1 and a string str2 as
+   arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1 (str): The first string to be added.
+        str2 (str): The first string to be added.
+
+    Returns:
+        str: a concatenated string
+    """
+    return str1 + str2
