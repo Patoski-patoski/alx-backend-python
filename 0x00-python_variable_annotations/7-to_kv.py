c8d92ccf3f608e89a766da570c7610f3b62b01fb
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-02 00:01:47 +0000

    takes a string k and an int OR float v as arguments and returns a tuple.

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""7-to_kv.py: Complex types - string and int/float to tuple"""
+
+from typing import List, Union, Iterator, Callable, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """t takes a string k and an int OR float v as arguments and returns
+       a tuple. The first element of the tuple is the string k.
+       The second element is the square of the int/float v and should
+       be annotated as a float.
+
+    Args:
+        k (str): The first element of the tuple
+        v (Union[int, float]): The second element is the square
+
+    Returns:
+        Tuple[str, Union[int, float]]: returns a tuple
+    """
+    return (str(k), v * v)
