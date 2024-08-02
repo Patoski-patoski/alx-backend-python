8a1f726abbbb24c4f88fd824ac00041f40276e92
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-02 00:56:12 +0000

    takes a float multiplier as argument and returns a function that multiplies a float by multiplier.

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""8-make_multiplier.py: Complex types - functions"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """a type-annotated function make_multiplier that takes a float multiplier
+       as argument and returns a function that multiplies a float by multiplier
+
+    Args:
+        multiplier (float):  a float multiplier
+
+    Returns:
+        Callable[[float], float]: returns a function that multiplies a float
+        by multiplier.
+    """
+    def multiply(num: multiplier) -> float:
+        result = num * num
+        return result
+
+    return multiply
