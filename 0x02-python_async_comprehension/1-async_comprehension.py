76c6869b5c5b09276b2a503f044f4350b4644a9a
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-08 04:36:29 +0000

    collect 10 random numbers using an async comprehensing. return the 10 random numbers.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,8 +2,7 @@
 """1-async_comprehension"""
 
 from typing import List
-
-async_generator = __import("0-async_generator").async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
