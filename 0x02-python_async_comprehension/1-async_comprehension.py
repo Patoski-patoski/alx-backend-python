1b81bda054b2e9f9210ed025d2a4589ac7426ba3
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-08 04:31:53 +0000

    collect 10 random numbers using an async comprehensing. return the 10 random numbers.

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""0-async_generator.py"""
+from typing import List
+
+async_generator = __import("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async_comprehension:  collect 10 random numbers using an async
+       comprehensing over async_generator
+
+    Returns:
+        List[float]: the 10 random numbers from async_generator()
+    """
+    odd_numbers = [i async for i in async_generator()]
+    return odd_numbers
