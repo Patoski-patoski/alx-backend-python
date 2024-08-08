e903047787bb80257fdb8e24c1523ac6ac19188b
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-08 03:44:13 +0000

    a coroutine called async_generator that takes no arguments.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""0-async_generator.py"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """async_generator:a coroutine called async_generator that takes no
+       arguments.
+
+    Yields:
+        _type_: a random number between 0 and 10
+    """
+    for _ in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
