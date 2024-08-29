4c7ccd1eff7f116b8361065d398185175fa4f209
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-29 14:28:00 +0000

    implement Mock setup for get_json()

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
-from typing import Any, Mapping, Sequence
+from typing import Any, Mapping, Sequence, Dict
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -39,6 +40,23 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(n_map, path)
 
         self.assertEqual(str(context.exception), f"{expected}")
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson: test and returns Mock HTTP calls"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict):
+        """test_get_json:to test that utils.get_json returns expected result"""
+
+        mock_resp = Mock()
+        mock_resp.json.return_value = test_payload
+        with patch("utils.requests.get", return_value=mock_resp) as mock_get:
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
