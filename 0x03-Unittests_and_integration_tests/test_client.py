2a1a2ab63bd0da0e6e0f859c4c9b836d0a952455
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-30 09:50:27 +0000

    memoize turns methods into properties.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """test_client module"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,6 +27,28 @@ class TestGithubOrgClient(unittest.TestCase):
         org_url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(org_url)
         self.assertEqual(result, expected_payload)
+
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google/repos"),
+        ("abc", "https://api.github.com/orgs/abc/repos"),
+    ])
+    def test_public_repos_url(self, org_name, expected_org_url: str):
+        """generates the puplic url for org_name"""
+
+        # Prepare the mock return value for the org property
+        payload = {"repos_url": expected_org_url}
+
+        # Patch GithubOrgClient.org to return the payload
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock, return_value=payload):
+            # create instance of GithubOrgClient
+            client = GithubOrgClient(org_name)
+
+            # call the _public_repos_url method
+            result = client._public_repos_url
+
+            # Assert that get_json was called once with the correct URL
+            self.assertEqual(result, expected_org_url)
 
 
 if __name__ == "__main__":
