3a6dec785d25457d0a7869e9bcff3b16fea9962c
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-30 08:48:10 +0000

    test that GithubOrgClient.org returns the correct value.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""test_client module"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test for GithubOrgClient class"""
+    @parameterized.expand([
+        ('google'),
+        ('abc'),
+    ])
+    @patch("client.get_json")
+    def test_method(self, org_name: str, mock_get_json):
+        """test that GithubOrgClient.org returns the correct value."""
+        # prepare the mock return value
+        expected_payload = {'org_name': org_name}
+        mock_get_json.return_value = expected_payload
+        # create instance of GithubOrgClient
+        client = GithubOrgClient(org_name)
+        # call the org method
+        result = client.org
+        # Assert that get_json was called once with the correct URL
+        ORG_URL = f"https://api.github.com/orgs/{org_name}"
+        mock_get_json.assert_called_once_with(ORG_URL)
+
+
+if __name__ == "__main__":
+    unittest.main()
